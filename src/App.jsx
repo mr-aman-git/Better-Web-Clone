0a49d0bd43a4049a87b96e7cb11fb395e244c876
mr-aman-git <amanazr2003@gmail.com>
2025-02-21 00:38:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
